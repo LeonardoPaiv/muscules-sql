23c4ffffff475a401c30b2848d733f502eb14666
LeonardoPaiv <leopaiva45@gmail.com>
2024-01-16 23:04:14 -0300

    add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Muscles SQL Instructions Support Repository
+
+This repository contains instructions for using SQL with the "Muscles" project. The instructions are provided in the form of SQL scripts that can be executed on your local machine or on a remote server.
+
+## Muscules Repository
+
+https://github.com/LeonardoPaiv/muscules
+
--- queries.sql
+++ queries.sql
@@ -29,30 +29,3 @@ JOIN
 ORDER BY id_exercicio_musculo_ativo ASC;
 
 
---- drop 
--- Tabela de relacionamento n para m entre "treino" e "exercicio_treino"
-DROP TABLE IF EXISTS treino_exercicio_treino;
-
--- Tabela "treino_classificacao"
-DROP TABLE IF EXISTS treino_classificacao;
-
--- Tabela "treino" (sem a chave cod_exercicio_treino)
-DROP TABLE IF EXISTS treino;
-
--- Tabela "exercicio_treino"
-DROP TABLE IF EXISTS exercicio_treino;
-
--- Tabela "metodo"
-DROP TABLE IF EXISTS metodo;
-
--- Tabela de relacionamento n para m entre "exercicio" e "musculo_ativo"
-DROP TABLE IF EXISTS exercicio_musculo_ativo;
-
--- Tabela "exercicio"
-DROP TABLE IF EXISTS exercicio;
-
--- Tabela "musculo_ativo"
-DROP TABLE IF EXISTS musculo_ativo;
-
--- Tabela "classificacao"
-DROP TABLE IF EXISTS classificacao;
